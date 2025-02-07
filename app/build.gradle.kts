3869ebdb7136c326f7f30e28aed8bfb1fa038029
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2025-02-07 13:27:22 +0900

    🐣

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,48 +1,28 @@
+import com.android.sdklib.AndroidVersion.VersionCodes.LOLLIPOP
+import com.android.sdklib.AndroidVersion.VersionCodes.VANILLA_ICE_CREAM
+
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.android)
 }
 
-android {
-    namespace = "com.example.weatherapp"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.example.weatherapp"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+repositories {
+    google()
+    mavenCentral()
 }
 
 dependencies {
+    implementation(libs.androidx.activity.compose)
+}
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+android {
+    namespace = "vn.ac.vju.mad.converter"
+    compileSdk = VANILLA_ICE_CREAM
+    defaultConfig {
+        minSdk = LOLLIPOP
+        targetSdk = VANILLA_ICE_CREAM
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
 }
---
+++ app/src/main/java/vn/ac/vju/mad/converter/MainActivity.kt
@@ -0,0 +1,5 @@
+package vn.ac.vju.mad.converter
+
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity()
